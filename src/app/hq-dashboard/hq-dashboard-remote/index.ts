3798b5f4eb13de07c97a6f8686abaf474e9c00b7
Rob Wormald <robwormald@gmail.com>
2017-09-28 16:41:19 -0700

    mock in admin and remote routes

---
+++ src/app/hq-dashboard/hq-dashboard-remote/index.ts
@@ -0,0 +1,1 @@
+export * from './ng_module'
--- src/app/hq-dashboard/hq-dashboard.routes.ts
+++ src/app/hq-dashboard/hq-dashboard.routes.ts
@@ -2,5 +2,19 @@ import {Route} from '@angular/router';
 import {HqDashboardView} from './hq-dashboard-view';
 
 export const HqDashboardRoutes:Route[] = [
-  { path: '', pathMatch: 'full', component: HqDashboardView}
+  {
+    path: '',
+    pathMatch: 'full',
+    component: HqDashboardView
+  },
+  {
+    path: 'admin',
+    pathMatch: 'full',
+    loadChildren: './hq-dashboard-admin/index#HqDashboardAdmin'
+  },
+  {
+    path: 'remote',
+    pathMatch: 'full',
+    loadChildren: './hq-dashboard-remote/index#HqDashboardRemote'
+  }
 ]
